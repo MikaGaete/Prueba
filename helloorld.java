7f989711ebb15c58c09f3d77aec5134c8264a343
MikaGaete <mikael.gaete@mail.udp.cl>
2022-04-12 16:54:57 -0400

    Metodo Adicional

--- helloorld.java
+++ helloorld.java
@@ -1,6 +1,12 @@
 class helloorld{
 	public static void main(String[] args){
 		System.out.println("Hola mundo");
+		
 	}
+	public Node<T> remove(T value){
+		Node<T> ret == null;
+		Node<T> tmp;
+		
+		if (this.head == null) { return null; }
 }
 
